a281519c6f1b7046b85756b19590837a0d176b45
Medina <Medina@LAPTOP>
2022-12-07 02:09:51 +0300

    Добавьте файлы проекта.

---
+++ PracticeStudy/Components/PartialClass/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeStudy.Components.PartialClass
+{
+    internal class Product
+    {
+        public bool IsActive { get; set; }
+        public string ActiveText
+        {
+            get
+            {
+                return (IsActive) ? "Актуален" : "Нет в наличии";
+            }
+        }
+    }
+}
